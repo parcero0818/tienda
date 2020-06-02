eae88472c06aad298a2161d058e91ce50b8d2b12
parcero0818 <john.sgv@outlook.com>
2020-06-01 21:08:57 -0500

    cambios

--- src/main/java/com/cidenet/tienda/repositorio/MovimientoRepositorio.java
+++ src/main/java/com/cidenet/tienda/repositorio/MovimientoRepositorio.java
@@ -2,7 +2,7 @@ package com.cidenet.tienda.repositorio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.cidenet.tienda.modelo.Movimiento;
+import com.cidenet.tienda.infraestructura.entidades.Movimiento;
 
 public interface MovimientoRepositorio extends JpaRepository<Movimiento, Long>{
 
--- src/main/java/com/cidenet/tienda/repositorio/ProductoRepositorio.java
+++ src/main/java/com/cidenet/tienda/repositorio/ProductoRepositorio.java
@@ -2,7 +2,7 @@ package com.cidenet.tienda.repositorio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.cidenet.tienda.modelo.Producto;
+import com.cidenet.tienda.infraestructura.entidades.Producto;
 
 public interface ProductoRepositorio extends JpaRepository<Producto, Long>{
 
